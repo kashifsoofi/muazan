b95bdfd3d6eaa8c489ae67190f9cde839eb5c829
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-09-30 21:48:57 +0100

    Android notification (#4)

---
+++ Muazan/NotificationEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Muazan
+{
+    public class NotificationEventArgs : EventArgs
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
