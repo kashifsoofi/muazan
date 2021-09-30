7206d045611aa6957a430b1325a3c221f8747938
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-09-30 21:48:14 +0100

    Android notification

--- Muazan.Android/MainActivity.cs
+++ Muazan.Android/MainActivity.cs
@@ -4,10 +4,18 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Content;
+using Xamarin.Forms;
 
 namespace Muazan.Droid
 {
-    [Activity(Label = "Muazan", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(
+        Label = "Muazan",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize,
+        LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -17,7 +25,24 @@ namespace Muazan.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            CreateNotificationFromIntent(Intent);
         }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            CreateNotificationFromIntent(intent);
+        }
+
+        void CreateNotificationFromIntent(Intent intent)
+        {
+            if (intent?.Extras != null)
+            {
+                string title = intent.GetStringExtra(AndroidNotificationManager.TitleKey);
+                string message = intent.GetStringExtra(AndroidNotificationManager.MessageKey);
+                DependencyService.Get<INotificationManager>().ReceiveNotification(title, message);
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- Muazan/MainPage.xaml.cs
+++ Muazan/MainPage.xaml.cs
@@ -10,19 +10,45 @@ namespace Muazan
 {
     public partial class MainPage : ContentPage
     {
+        INotificationManager notificationManager;
+        int notificationNumber = 0;
+
         public MainPage()
         {
             InitializeComponent();
+
+            notificationManager = DependencyService.Get<INotificationManager>();
+            notificationManager.NotificationReceived += (sender, eventArgs) =>
+            {
+                var evtData = (NotificationEventArgs)eventArgs;
+                ShowNotification(evtData.Title, evtData.Message);
+            };
         }
 
-        public void PlayFajrAdhan_Clicked(System.Object sender, System.EventArgs e)
+        public void OnPlayFajrAdhanClicked(System.Object sender, System.EventArgs e)
         {
             PlayAdhan(true);
         }
 
-        void PlayAdhan_Clicked(System.Object sender, System.EventArgs e)
+        void OnPlayAdhanClicked(System.Object sender, System.EventArgs e)
         {
             PlayAdhan(false);
+        }
+
+        void OnSendClick(System.Object sender, System.EventArgs e)
+        {
+            notificationNumber++;
+            string title = $"Local Notification #{notificationNumber}";
+            string message = $"You have now received {notificationNumber} notifications!";
+            notificationManager.SendNotification(title, message);
+        }
+
+        void OnScheduleClick(System.Object sender, System.EventArgs e)
+        {
+            notificationNumber++;
+            string title = $"Local Notification #{notificationNumber}";
+            string message = $"You have now received {notificationNumber} notifications!";
+            notificationManager.SendNotification(title, message, DateTime.Now.AddSeconds(10));
         }
 
         private void PlayAdhan(bool isFajr)
@@ -30,6 +56,18 @@ namespace Muazan
             var prefix = isFajr ? "Fajr-" : "";
             var fileName = $"{prefix}Adhan-Makkah.mp3";
             DependencyService.Get<IAudioPlayer>().PlayAudioFile(fileName);
+        }
+
+        private void ShowNotification(string title, string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Label()
+                {
+                    Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+                };
+                stackLayout.Children.Add(msg);
+            });
         }
     }
 }
