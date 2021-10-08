1719235b0c93139fcd15fb6b2f111d9a2e196bfa
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-10-09 00:20:04 +0100

    Added Location, show namaz time on startup

--- Muazun/MainPage.xaml.cs
+++ Muazun/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Muazun.Services;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Muazun
@@ -21,17 +23,43 @@ namespace Muazun
             };
         }
 
-        public void OnPlayFajrAdhanClicked(System.Object sender, System.EventArgs e)
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var currentDate = DateTime.Now;
+            lblCurrentDate.Text = currentDate.ToLongDateString();
+
+            var location = Geolocation.GetLastKnownLocationAsync().GetAwaiter().GetResult();
+            var namazTime = new NamazTime();
+            namazTime.SetCalcMethod(2);
+            namazTime.SetAsrMethod(0);
+
+            var tz = (DateTime.UtcNow - currentDate).Hours;
+            var times = namazTime.GetDatePrayerTimes(currentDate.Year, currentDate.Month, currentDate.Day, location.Longitude, location.Latitude, tz);
+            lblFajr.Text = times[0];
+            lblSunrise.Text = times[1];
+            lblDhuhr.Text = times[2];
+            lblAsr.Text = times[3];
+            lblSunset.Text = times[4];
+            lblMaghrib.Text = times[5];
+            lblIsha.Text = times[6];
+
+            // Set notification for next namaz
+            // notificationService.SendNotification("Namaz", "Time for Namaz", false, DateTime.Now.AddSeconds(10));
+        }
+
+        public void OnPlayFajrAdhanClicked(object sender, EventArgs e)
         {
             PlayAdhan(true);
         }
 
-        void OnPlayAdhanClicked(System.Object sender, System.EventArgs e)
+        void OnPlayAdhanClicked(object sender, EventArgs e)
         {
             PlayAdhan(false);
         }
 
-        void OnSendClick(System.Object sender, System.EventArgs e)
+        void OnSendClick(object sender, EventArgs e)
         {
             notificationNumber++;
             string title = $"Local Notification #{notificationNumber}";
@@ -39,12 +67,17 @@ namespace Muazun
             notificationService.SendNotification(title, message, true);
         }
 
-        void OnScheduleClick(System.Object sender, System.EventArgs e)
+        void OnScheduleClick(object sender, EventArgs e)
         {
             notificationNumber++;
             string title = $"Local Notification #{notificationNumber}";
             string message = $"You have now received {notificationNumber} notifications!";
             notificationService.SendNotification(title, message, false, DateTime.Now.AddSeconds(10));
+        }
+
+        async void OnGetLocationClick(object sender, EventArgs e)
+        {
+            await GetLocation();
         }
 
         private void PlayAdhan(bool isFajr)
@@ -64,6 +97,32 @@ namespace Muazun
                 };
                 stackLayout.Children.Add(msg);
             });
+        }
+
+        async Task GetLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                var location = await Geolocation.GetLocationAsync(request);
+                if (location != null)
+                {
+                    lblLatitude.Text = location.Latitude.ToString();
+                    lblLongitude.Text = location.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                await DisplayAlert("Failed", ex.Message, "OK");
+            }
+            catch (PermissionException ex)
+            {
+                await DisplayAlert("Failed", ex.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Failed", ex.Message, "OK");
+            }
         }
     }
 }
