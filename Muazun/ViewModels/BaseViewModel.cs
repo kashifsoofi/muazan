a881aa5f2636bc4068213afb2c28f4d9a19a5241
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-10-24 19:37:20 +0100

    ViewModel

---
+++ Muazun/ViewModels/BaseViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Muazun.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public const string PageTitlePropertyName = "PageTitle";
+
+        private string pageTitle;
+        public string PageTitle
+        {
+            get => pageTitle;
+            set { pageTitle = value; OnPropertyChanged(); }
+        }
+
+        protected BaseViewModel()
+        { }
+
+        public abstract Task Init();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public abstract class BaseViewModel<TParam> : BaseViewModel
+    {
+        protected BaseViewModel()
+        { }
+    }
+}
